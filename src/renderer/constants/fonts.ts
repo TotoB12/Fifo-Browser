23a8929efa610e21f59c7a6bcc3c3dd5f42d7da0
TotoB12 <91705868+TotoB12@users.noreply.github.com>
2023-08-16 12:24:02 -0400

    Update fonts.ts

--- src/renderer/constants/fonts.ts
+++ src/renderer/constants/fonts.ts
@@ -1,5 +1,5 @@
 /* Copyright (c) 2021-2022 SnailDOS */
 
-export const FONT_ROBOTO_REGULAR = require('~/renderer/resources/fonts/inter-regular.woff2');
-export const FONT_ROBOTO_MEDIUM = require('~/renderer/resources/fonts/inter-medium.woff2');
-export const FONT_ROBOTO_LIGHT = require('~/renderer/resources/fonts/inter-light.woff2');
+export const FONT_INTER_REGULAR = require('~/renderer/resources/fonts/inter-regular.woff2');
+export const FONT_INTER_MEDIUM = require('~/renderer/resources/fonts/inter-medium.woff2');
+export const FONT_INTER_LIGHT = require('~/renderer/resources/fonts/inter-light.woff2');
